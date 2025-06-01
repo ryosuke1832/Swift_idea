912d6e005d82aa186d0f9ee53b2f0220d1bb4321
ryosuke1832 <84903480+ryosuke1832@users.noreply.github.com>
2025-06-01 22:49:49 +1000

    initial

---
+++ ideasTests/ideasTests.swift
@@ -0,0 +1,17 @@
+//
+//  ideasTests.swift
+//  ideasTests
+//
+//  Created by user on 2025/06/01.
+//
+
+import Testing
+@testable import ideas
+
+struct ideasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
